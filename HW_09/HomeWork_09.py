5b51e55ffd36fb790d193dc25cddece083b68ab5
Andrii V <kidstonek@gmail.com>
2022-05-23 14:43:58 +0300

    Start of main HW 09 - ver 0.3 added function for command check + strip decorator

--- HW_09/HomeWork_09.py
+++ HW_09/HomeWork_09.py
@@ -15,6 +15,10 @@
 
 "good bye", "close", "exit" по любой из этих команд бот завершает свою роботу после того,
 как выведет в консоль "Good bye!".
+
+
+Доволі складно читається Ваш декоратор, Ви туди вписали також і парсер даних. Винесіть цей код в іншу функцію.
+Також обробку команл варто зробити в зовнішній функції, тоді і блок `main` - буде краще читатись)
 """
 
 MY_PHONEBOOK = {}
@@ -23,17 +27,6 @@ MY_PHONEBOOK = {}
 def input_error(in_func):
     def wrapper(some_string):
         try:
-            if str(some_string.split(' ')[0]) == 'add':
-                tmp = str(some_string.split(' ')[1])
-                for i in filter(lambda x: bool(x == tmp), MY_PHONEBOOK.keys()):
-                    return "Such a name already exists. Please try again"
-            if str(some_string.split(' ')[1]).isdigit() or str(some_string.split(' ')[1]) == '':
-                return "Incorrect user name. Try again"
-            if str(some_string.split(' ')[0]) == 'phone':
-                check = in_func(some_string)
-                return check
-            if not str(some_string.split(' ')[2]).isdigit() or str(some_string.split(' ')[2]).isdigit() == '':
-                return "Incorrect phone number. Try again"
             check = in_func(some_string)
             return check
         except KeyError:
@@ -47,14 +40,22 @@ def input_error(in_func):
     return wrapper
 
 
+def handler_hello():
+    return 'How can I help you?'
+
+
 @input_error
 def handler_adder(add_string: str):
+    if not str(add_string.lower().split(' ')[0]) == 'add':
+        return add_string
     MY_PHONEBOOK[str(add_string.split(' ')[1])] = str(add_string.split(' ')[2])
     return 'Contact ' + str(add_string.split(' ')[1]) + ' was added with phone number ' + str(add_string.split(' ')[2])
 
 
 @input_error
 def handler_changer(change_string: str):
+    if not str(change_string.lower().split(' ')[0]) == 'change':
+        return change_string
     MY_PHONEBOOK[str(change_string.split(' ')[1])] = str(change_string.split(' ')[2])
     return 'The phone number for contact ' + str(change_string.split(' ')[1]) + ' was changed'
 
@@ -65,24 +66,36 @@ def handler_phone_show(phone_show_string: str):
            + MY_PHONEBOOK[str(phone_show_string.split(' ')[1])]
 
 
+@input_error
+def command_checker(command_string: str):
+    if str(command_string.lower().split(' ')[0]) == 'add':
+        tmp = str(command_string.split(' ')[1])
+        for i in filter(lambda x: bool(x == tmp), MY_PHONEBOOK.keys()):
+            return "Such a name already exists. Please try again"
+    if str(command_string.lower().split(' ')[1]).isdigit() or str(command_string.lower().split(' ')[1]) == '':
+        return "Incorrect user name. Try again"
+    if str(command_string.lower().split(' ')[0]) == 'phone':
+        return command_string
+    if not str(command_string.split(' ')[2]).isdigit() or str(command_string.split(' ')[2]).isdigit() == '':
+        return "Incorrect phone number. Try again"
+    else:
+        return command_string
+
+
+USERS_COMMANDS = {handler_hello: 'hello', handler_phone_show: 'phone', handler_changer: 'change', handler_adder: 'add'}
+
+
 def main():
     while True:
         tmp = input('Please input command: ')
         if tmp.lower() == 'good bye' or tmp.lower() == 'close' or tmp.lower() == 'exit':
             print("Good bye!")
             break
-        elif tmp.lower() == 'hello':
-            print('How can I help you?')
-        elif tmp.lower() == 'show all':
+        if tmp.lower() == 'show all':
             print(MY_PHONEBOOK)
-        elif tmp.lower().split(' ')[0] == 'add':
-            print(handler_adder(tmp))
-        elif tmp.lower().split(' ')[0] == 'change':
-            print(handler_changer(tmp))
-        elif tmp.lower().split(' ')[0] == 'phone':
-            print(handler_phone_show(tmp))
-        else:
-            print('There is no such a command. Please try again')
+        for k, v in USERS_COMMANDS.items():
+            if v == tmp.lower().split(' ')[0]:
+                print(k(command_checker(tmp)))
 
 
 if '__main__' == __name__:
